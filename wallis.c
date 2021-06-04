b2970b628d1e28c7397d7e1de7b99de1323b159b
Nacn3 <84712141+Nacn3@users.noreply.github.com>
2021-06-04 13:56:38 +0530

    Commit 1

--- wallis.c
+++ wallis.c
@@ -3,17 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int n)
-{float pi=1.0;
-for (int i=1;i<=n; i++)
-  {
-  int m;
-  m=4.0*i*i;
-  pi=(float)((pi*m)/(m-1));
-}
-  pi=pi *2.0;
-    return pi;
-}
+float wallis_pi(int);
 
 int main(void) {
   float pi;
@@ -32,3 +22,13 @@ int main(void) {
       abort();
     }
   }
+}
+ float wallis_pi(int n)
+ {
+    float s=1.0;
+    for (int i=1; i<=n; i++)
+    {
+        s=s*(4*i*i)/((4*i*i)-1);
+    }
+   return (2*s); 
+ }
